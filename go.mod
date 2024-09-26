ab3db68f2db50158d7325daf086f41667588427b
jyo@TI221167 <bhawya@loyalistcollege.com>
2024-09-26 16:45:03 -0400

    Changing main file and calling functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bhawya97/group_G_week_4
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Group G's Week 4 Project!")
+
+	fmt.Println(add(1, 2))
+	fmt.Println(sub(5, 3))
+	fmt.Println(div(10, 2))
+	fmt.Println(multi(10, 2))
+
+}
